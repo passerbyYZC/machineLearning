da4b715d495b0db9b547caf61d0a7593d6dcd9e4
yao <yaozhichao418@163.com>
2019-11-04 13:42:06 +0800

    KNN学习笔记及代码

---
+++ kNN/KNN.py
@@ -0,0 +1,36 @@
+# encoding:utf-8
+
+import numpy as np
+import operator
+
+def createDataSet():
+    """创建数据集以及标签
+    """
+    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+def classify0(inX, dataSet, labels, k):
+    """样本分类
+    Args:
+        inX: 用于分类的输入向量
+        dataSet: 训练样本集
+        labels: 标签向量
+        k: 选择最近邻居数目
+    
+    Return:
+        输入向量所属类别
+    """
+
+    dataSetSize = dataSet.shape[0]
+
+    # 距离计算并升序排序
+    diffMat = np.tile(inX, (dataSetSize,1)) - dataSetSize
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5    
+    sortedDistIndicies = distances.argsort()
+
+    # 计算最近临近类别频率
+    classCount = {}
+    
