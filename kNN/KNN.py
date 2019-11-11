0f2b00c9c404974864e43ede964e5a8cee63f05e
yao <yaozhichao418@163.com>
2019-11-11 16:13:36 +0800

    KNN算法完全实现

--- kNN/KNN.py
+++ kNN/KNN.py
@@ -3,15 +3,10 @@
 import numpy as np
 import operator
 
-def createDataSet():
-    """创建数据集以及标签
-    """
-    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
-    labels = ['A', 'A', 'B', 'B']
-    return group, labels
 
 def classify0(inX, dataSet, labels, k):
     """样本分类
+
     Args:
         inX: 用于分类的输入向量
         dataSet: 训练样本集
@@ -39,5 +34,45 @@ def classify0(inX, dataSet, labels, k):
         sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(i), reverse=True)
     return sortedClassCount[0][0]
 
-group, labels = createDataSet()
-print(classify0([1.0,1.1], group, labels, 2))
+def file2matrix(filename):
+    """读取文本数据并结构化
+
+    Args:
+        filename: 文本文件路径
+    
+    Return:
+        数据样本矩阵，样本标签向量
+    """
+    with open(filename, "r") as fr:
+        arrayOlines = fr.readlines()
+        numberOfLines = len(arrayOlines)  # 样本数
+
+    returnMat = np.zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    # 解析数据
+    for line in arrayOlines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+def autoNorm(dataSet):
+    """归一化特征值
+
+    Args:
+        dataSet: 样本数据集
+
+    Return:
+        归一化样本数据集、范围宽带、最小值
+    """
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = np.zeros(np.shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - np.tile(minVals, (m,1))
+    normDataSet = normDataSet/np.tile(ranges, (m,1))
+    return normDataSet, range, minVals
---
+++ kNN/demp.py
@@ -0,0 +1,18 @@
+import KNN
+import matplotlib
+import matplotlib.pyplot as plt
+from numpy import array
+
+
+filePath = r"C:\Users\Yaozh\Documents\VSCode\github\machineLearning\machineLearning\kNN\datingTestSet2.txt"
+data, labels = KNN.file2matrix(filePath)
+normMat, range, minVals = KNN.autoNorm(data)
+data = normMat
+fig = plt.figure()
+ax = fig.add_subplot(131)
+ax.scatter(data[:, 1], data[:, 2], 15.0*array(labels), 15.0*array(labels))
+ax = fig.add_subplot(132)
+ax.scatter(data[:, 0], data[:, 2], 15.0*array(labels), 15.0*array(labels))
+ax = fig.add_subplot(133)
+ax.scatter(data[:, 0], data[:, 1], 15.0*array(labels), 15.0*array(labels))
+plt.show()
