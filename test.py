3de60f4f7272bd31a6c425074f823d46201c3b44
Yao Z.C <yaozhichao418@163.com>
2021-07-01 14:49:10 +0800

    初始化

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 """
     Author: Yao Z.C.
     Date: 2021-07-01 14:38:10
-    LastEditTime: 2021-07-01 14:38:11
+    LastEditTime: 2021-07-01 14:46:33
     FilePath: /machineLearning/test.py
 """
+print("Hello python world!")
